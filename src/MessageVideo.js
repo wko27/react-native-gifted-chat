194fea7684bd4eee132ab1b620496763bece4e87
wko27 <wing.hong.a.ko@gmail.com>
2017-12-05 02:07:26 -0800

    switch to native player

--- src/MessageVideo.js
+++ src/MessageVideo.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
+  Dimensions,
   Image,
   StyleSheet,
   TouchableOpacity,
   ViewPropTypes,
 } from 'react-native';
 import Lightbox from 'react-native-lightbox';
-import Video from 'react-native-video';
+import VideoPlayer from 'react-native-native-video-player';
 
 export default class MessageVideo extends React.Component {
   constructor(props) {
@@ -18,10 +19,7 @@ export default class MessageVideo extends React.Component {
   }
 
   onVideoClick() {
-    console.log("Playing in full screen")
-    console.log(this.player);
-    this.setState({isPlaying: !this.state.isPlaying});
-    this.player.presentFullscreenPlayer();
+    VideoPlayer.showVideoPlayer(this.props.currentMessage.video);
   }
   
   render() {
@@ -30,39 +28,10 @@ export default class MessageVideo extends React.Component {
         onPress={this.onVideoClick.bind(this)}
         style={[styles.container, this.props.containerStyle]}
       >
-        <Lightbox
-          activeProps={{
-            style: styles.videoActive,
-          }}
-          {...this.props.lightboxProps}
-        >
-          <Image
-            {...this.props.videoProps}
-            style={[styles.video, this.props.videoStyle]}
-            source={{uri: this.props.currentMessage.video}}
-          />
-        </Lightbox>
-        <Video
+        <Image
+          {...this.props.videoProps}
+          style={[styles.video, this.props.videoStyle]}
           source={{uri: this.props.currentMessage.video}}
-          ref={(ref) => {this.player = ref}}       // Store reference
-          rate={1.0}                              // 0 is paused, 1 is normal.
-          volume={1.0}                            // 0 is muted, 1 is normal.
-          muted={false}                           // Mutes the audio entirely.
-          paused={!this.state.isPlaying}                          // Pauses playback entirely.
-          resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
-          repeat={false}                           // Repeat forever.
-          playInBackground={false}                // Audio continues to play when app entering background.
-          playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
-          ignoreSilentSwitch={"obey"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
-          progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
-          onLoadStart={this.loadStart}            // Callback when video starts to load
-          onLoad={this.setDuration}               // Callback when video loads
-          onProgress={this.setTime}               // Callback every ~250ms with currentTime
-          onEnd={this.onEnd}                      // Callback when playback finishes
-          onError={this.videoError}               // Callback when video cannot be loaded
-          onBuffer={this.onBuffer}                // Callback when remote video is buffering
-          onTimedMetadata={this.onTimedMetadata}  // Callback when the stream receive some metadata
-          style={this.state.isPlaying ? styles.hiddenVideo : styles.backgroundVideo}
         />
       </TouchableOpacity>
     );
@@ -78,24 +47,6 @@ const styles = StyleSheet.create({
     borderRadius: 13,
     margin: 3,
     resizeMode: 'cover',
-  },
-  videoActive: {
-    flex: 1,
-    resizeMode: 'contain',
-  },
-  hiddenVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
   },
 });
 
